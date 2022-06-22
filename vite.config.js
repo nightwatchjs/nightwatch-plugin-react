e2664be872ec085e5913f075d9456626ba9e3045
YevhenKap <kapelianovych.y.v@gmail.com>
2022-06-22 15:22:38 +0300

    Added support for React 18.

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import nightwatchPlugin from 'vite-plugin-nightwatch'
+import {shouldNewDOMAPIBeUsed} from './lib/react_utils.js';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   optimizeDeps: {
-    include: ['react', 'react-dom']
+    include: ['react', shouldNewDOMAPIBeUsed() ? 'react-dom/client' : 'react-dom']
   },
   plugins: [
     react(),
