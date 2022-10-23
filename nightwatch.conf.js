d7e3f9ac51543cc0ef5a108d0815361cf3eccb13
Andrei Rusu <andrei.rusu@beatfactor.net>
2022-10-23 15:30:47 +0200

    updated vite plugin

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,5 +1,3 @@
-// https://nightwatchjs.org/gettingstarted/configuration/
-
 module.exports = {
   // An array of folders (excluding subfolders) where your tests are located;
   // if this is not specified, the test source must be passed as the second argument to the test runner.
